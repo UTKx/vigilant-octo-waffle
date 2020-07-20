80231dfeb8cdf9979d161df19981c482db329b6d
UTKx <utkarshkumark@gmail.com>
2020-07-21 01:30:13 +0530

    created templates

--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -3,32 +3,32 @@ from . forms import TodoListForm, TodoForm
 from . models import TodoList, Todo
 
 # Create your views here.
+def list(request):
+    lists = TodoList.objects.all()
+    return render(request, 'main.html', {'lists':lists})
 
 def index(request):
     form = TodoListForm()
     lists = TodoList.objects.all()
-     if request.method =='POST':
-	    form = TodoListForm(request.POST)
-	    if form.is_valid():
-		    form.save()
+    if request.method =='POST':
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('show')
     else:
         form = TodoListForm()
 
     context = {'form': form, 'lists': lists}
-    return render(request, 'todo/index.html', context)
-
-def getList(request):
-    pass
+    return render(request, 'todoapp/index.html', context)
 
 def getOrCreateTodo(request, todolist_id):
     lists = TodoList.objects.get(id=todolist_id)
     todos = Todo.objects.filter(todolist__id=todolist_id)
     form = TodoForm()
     if request.method =='POST':
-	    form = TodoForm(request.POST)
-	    if form.is_valid():
-		    form.save()
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('show')
     else:
         form = TodoForm()
