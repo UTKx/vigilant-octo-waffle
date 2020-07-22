a9aad32d3ebeed9c39bfeed2acfa4890af23f131
UTKx <utkarshkumark@gmail.com>
2020-07-22 22:48:33 +0530

    working on frontend

--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -3,13 +3,12 @@ from . forms import TodoListForm, TodoForm
 from . models import TodoList, Todo
 
 # Create your views here.
-def list(request):
-    lists = TodoList.objects.all()
-    return render(request, 'main.html', {'lists':lists})
 
 def index(request):
-    form = TodoListForm()
+    listform = TodoListForm()
+    todoform = TodoForm()
     lists = TodoList.objects.all()
+    tasks = Todo.objects.filter(todolist__name='Task')
     if request.method =='POST':
         form = TodoListForm(request.POST)
         if form.is_valid():
@@ -18,13 +17,6 @@ def index(request):
     else:
         form = TodoListForm()
 
-    context = {'form': form, 'lists': lists}
-    return render(request, 'todoapp/index.html', context)
-
-def getOrCreateTodo(request, todolist_id):
-    lists = TodoList.objects.get(id=todolist_id)
-    todos = Todo.objects.filter(todolist__id=todolist_id)
-    form = TodoForm()
     if request.method =='POST':
         form = TodoForm(request.POST)
         if form.is_valid():
@@ -33,5 +25,30 @@ def getOrCreateTodo(request, todolist_id):
     else:
         form = TodoForm()
 
-    context = {'form': form, 'lists': lists, 'todos': todos}
-    return render(request, 'todo/showtodo.html', context)
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'tasks': tasks}
+    return render(request, 'todoapp/index.html', context)
+
+def getOrCreateTodo(request, todolist_id):
+    lists = TodoList.objects.all()
+    curlist = TodoList.objects.get(id=todolist_id)
+    todos = Todo.objects.filter(todolist__id=todolist_id)
+    todoform = TodoForm()
+    listform = TodoListForm()
+    if request.method =='POST':
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show')
+    else:
+        form = TodoListForm()
+
+    if request.method =='POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('show')
+    else:
+        form = TodoForm()
+
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'todos': todos}
+    return render(request, 'todoapp/showtodo.html', context)
