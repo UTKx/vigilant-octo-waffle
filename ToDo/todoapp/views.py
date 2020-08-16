4f9f635c49d181002b7ed0807f9cce17504c3ee0
UTKx <utkarshkumark@gmail.com>
2020-08-17 01:08:04 +0530

    todo form implemented

--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -8,7 +8,10 @@ def index(request):
     listform = TodoListForm()
     todoform = TodoForm()
     lists = TodoList.objects.all()[2:]
-    tasks = Todo.objects.filter(todolist__name='Task')
+    curlist = TodoList.objects.get(id=9)
+    tasks = Todo.objects.filter(todolist__id='9')
+    task_count = Todo.objects.filter(todolist__id=9).count()
+    
     if request.method =='POST':
         form = TodoListForm(request.POST)
         if form.is_valid():
@@ -20,18 +23,20 @@ def index(request):
     if request.method =='POST':
         form = TodoForm(request.POST)
         if form.is_valid():
-            form.save()
-            # return redirect('show')
+            title = form.cleaned_data['title']
+            instance = Todo(title=title, todolist_id='9')
+            instance.save()
     else:
         form = TodoForm()
 
-    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'tasks': tasks}
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'tasks': tasks, 'task_count': task_count}
     return render(request, 'todoapp/index.html', context)
 
 def getOrCreateTodo(request, todolist_id):
     lists = TodoList.objects.all()[2:]
     curlist = TodoList.objects.get(id=todolist_id)
     todos = Todo.objects.filter(todolist__id=todolist_id)
+    todo_count = Todo.objects.filter(todolist__id=todolist_id).count()
     todoform = TodoForm()
     listform = TodoListForm()
     if request.method =='POST':
@@ -45,10 +50,11 @@ def getOrCreateTodo(request, todolist_id):
     if request.method =='POST':
         form = TodoForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('show')
+            title = form.cleaned_data['title']
+            instance = Todo(title=title, todolist_id=todolist_id)
+            instance.save()
     else:
         form = TodoForm()
 
-    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'todos': todos}
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'todos': todos, 'todo_count': todo_count}
     return render(request, 'todoapp/showtodo.html', context)
