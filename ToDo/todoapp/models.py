c4119375738ff1a6509808b50bec2aa558c0f2f3
UTKx <utkarshkumark@gmail.com>
2020-08-21 02:05:53 +0530

    added todo count

--- ToDo/todoapp/models.py
+++ ToDo/todoapp/models.py
@@ -8,11 +8,14 @@ class TodoList(models.Model):
     def __str__(self):
         return self.name
 
+    def count(self):
+        return self.todos.count()
+
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now=True)
     is_finished = models.BooleanField(default=False)
-    todolist = models.ForeignKey(TodoList, on_delete=models.CASCADE)
+    todolist = models.ForeignKey(TodoList, related_name='todos',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- ToDo/todoapp/views.py
+++ ToDo/todoapp/views.py
@@ -4,6 +4,7 @@ from . models import TodoList, Todo
 
 # Create your views here.
 
+
 def index(request):
     listform = TodoListForm()
     todoform = TodoForm()
@@ -11,16 +12,16 @@ def index(request):
     curlist = TodoList.objects.get(id=9)
     tasks = Todo.objects.filter(todolist__id='9')
     task_count = Todo.objects.filter(todolist__id=9).count()
-    
-    if request.method =='POST':
+    imp_count = Todo.objects.filter(todolist__id=10).count()
+
+    if request.method == 'POST':
         form = TodoListForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect('show')
     else:
         form = TodoListForm()
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
@@ -29,8 +30,10 @@ def index(request):
     else:
         form = TodoForm()
 
-    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'tasks': tasks, 'task_count': task_count}
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists,
+               'curlist': curlist, 'tasks': tasks, 'task_count': task_count, 'imp_count': imp_count}
     return render(request, 'todoapp/index.html', context)
+
 
 def getOrCreateTodo(request, todolist_id):
     lists = TodoList.objects.all()[2:]
@@ -39,7 +42,7 @@ def getOrCreateTodo(request, todolist_id):
     todo_count = Todo.objects.filter(todolist__id=todolist_id).count()
     todoform = TodoForm()
     listform = TodoListForm()
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TodoListForm(request.POST)
         if form.is_valid():
             form.save()
@@ -47,7 +50,7 @@ def getOrCreateTodo(request, todolist_id):
     else:
         form = TodoListForm()
 
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TodoForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data['title']
@@ -56,5 +59,6 @@ def getOrCreateTodo(request, todolist_id):
     else:
         form = TodoForm()
 
-    context = {'listform': listform, 'todoform': todoform, 'lists': lists, 'curlist': curlist, 'todos': todos, 'todo_count': todo_count}
+    context = {'listform': listform, 'todoform': todoform, 'lists': lists,
+               'curlist': curlist, 'todos': todos, 'todo_count': todo_count}
     return render(request, 'todoapp/showtodo.html', context)
