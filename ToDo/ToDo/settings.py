1157e7dbb7835af27935e3a04dd45eb97edefb23
Utkarsh <utkarshkumark@gmail.com>
2020-09-08 23:43:34 +0530

    migration done

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ToDo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
